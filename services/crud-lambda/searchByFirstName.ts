25ddd7f52efa99cb551342ac0519471918b4d873
Pasha <psmolov@nautilusdigital.com>
2023-02-28 13:58:35 -0500

    feat: add 4 lambdas for searching creterias: first, last names, email, city

---
+++ services/crud-lambda/searchByFirstName.ts
@@ -0,0 +1,40 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import * as mysql from 'mysql2/promise';
+
+const RDS_HOST = 'database-2.cmuvxrqhgxjx.us-east-1.rds.amazonaws.com';
+const RDS_PORT = 3306;
+const RDS_USER = 'admin';
+const RDS_PASSWORD = 'NonaNona';
+const RDS_DATABASE = 'reservationsDb';
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const connection = await mysql.createConnection({
+    host: RDS_HOST,
+    port: RDS_PORT,
+    user: RDS_USER,
+    password: RDS_PASSWORD,
+    database: RDS_DATABASE,
+  });
+
+  const searchTerm = event.queryStringParameters?.firstName || '';
+
+  try {
+    const [rows] = await connection.execute<mysql.RowDataPacket[]>(
+      `SELECT * FROM reservations WHERE first_name LIKE ?`,
+      [`${searchTerm}%`]
+    );
+    console.log(`Retrieved ${rows.length} reservations`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(rows)
+    };
+  } catch (error) {
+    console.error(`Error reading reservations: ${error}`);
+    return {
+      statusCode: 500,
+      body: 'Error reading reservations'
+    };
+  } finally {
+    connection.end();
+  }
+};
